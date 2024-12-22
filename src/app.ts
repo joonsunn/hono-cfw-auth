1104e4042f7ab92b6c2d4fabeb0e84bdbb22fd82
Joon Sunn Foo <joonsunn.foo@gmail.com>
2024-12-22 22:01:56 +0800

    test

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(envInjector());
 app.use(logger());
 app.use("*", async (c, next) => {
   const corsMiddleware = cors({
-    origin: "*",
+    origin: (origin) => origin,
     allowMethods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
     credentials: true,
   });
